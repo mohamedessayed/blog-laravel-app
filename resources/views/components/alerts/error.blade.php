313a69989e3b06c2516014f10c841a053a28c73e
mohamedessayed <mlovely@gmail.com>
2025-05-12 20:44:44 +0300

    system create and view all proccess is created

--- resources/views/components/alerts/error.blade.php
+++ resources/views/components/alerts/error.blade.php
@@ -1,7 +1,7 @@
 <div class="mb-3">
     <!-- He who is contented is rich. - Laozi -->
 @if (session('errorMassage'))
-    <div class="alert alert-success">
+    <div class="alert alert-danger">
         {{ session('errorMassage') }}
     </div>
 @endif
