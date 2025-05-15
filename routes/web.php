afc473cff7298f246652ea26fc80216967c9af20
mohamedessayed <mlovely@gmail.com>
2025-05-15 21:26:39 +0300

    add system api

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\WebsiteController;
 use App\Http\Middleware\UserAuth;
 use Illuminate\Support\Facades\Route;
@@ -23,15 +24,18 @@ Route::get('/logout',[AuthController::class,'logout'])->name('logout')->middlewa
 
 Route::middleware(UserAuth::class)->group(function(){
 
-Route::get('books',[BookController::class,'index'])->name('book.index');
-Route::get('book/create',[BookController::class,'create'])->name('book.create');
-Route::post('book/store',[BookController::class,'store'])->name('book.store');
-Route::delete('book/destory/{book}',[BookController::class,'destroy'])->whereNumber('book')->name('book.delete');
-Route::get('book/view/{book}',[BookController::class,'show'])->whereNumber('book')->name('book.view');
-Route::get('book/edit/{book}',[BookController::class,'edit'])->whereNumber('book')->name('book.edit');
-Route::put('book/update/{book}',[BookController::class,'update'])->whereNumber('book')->name('book.update');
 
+Route::prefix('dashboard')->group(function(){
+    Route::resource('user',UserController::class);
+    Route::get('book',[BookController::class,'index'])->name('book.index');
+    Route::get('book/create',[BookController::class,'create'])->name('book.create');
+    Route::post('book/store',[BookController::class,'store'])->name('book.store');
+    Route::delete('book/destory/{book}',[BookController::class,'destroy'])->whereNumber('book')->name('book.delete');
+    Route::get('book/view/{book}',[BookController::class,'show'])->whereNumber('book')->name('book.view');
+    Route::get('book/edit/{book}',[BookController::class,'edit'])->whereNumber('book')->name('book.edit');
+    Route::put('book/update/{book}',[BookController::class,'update'])->whereNumber('book')->name('book.update');
 
+});
 
 });
 
